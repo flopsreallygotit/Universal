cfa4ef9a8251912d90ea307676f6b986bbbfc3c4
flopsreallygotit <flopsreallygotit@gmail.com>
2022-12-24 21:06:58 +0300

    Initial commit

---
+++ universalUtils.cpp
@@ -0,0 +1,115 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#include "universalUtils.h"
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void logf (const char *file, const int line, const char *function,
+           FILE *output, const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    fprintf(output, BOLD MAGENTA "%s:%d | %s | " RESET,
+            file, line, function);
+            
+    vfprintf(output, format, args);
+
+    va_end(args);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void *recalloc (void *ptr, size_t newSize) 
+{
+    size_t oldSize = malloc_usable_size(ptr);
+
+    ptr = realloc(ptr, newSize);
+    char *endptr = (char *) ptr;
+
+    for (size_t idx = oldSize; idx < newSize; idx++) 
+        endptr[idx] = 0;
+
+    return ptr;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+char *stralloccat(const char *dst, const char *src)
+{
+    char *string = (char *) calloc (strlen(dst) + strlen(src) + 1, sizeof(char));
+
+    strcpy(string, dst);
+    strcat(string, src);
+
+    return string;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int systemf (const char *format, ...)
+{
+    va_list arg;
+    va_start(arg, format);
+
+    char str[512] = "";
+
+    vsprintf(str, format, arg); 
+
+    va_end(arg);
+
+    return system(str);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int differenceSign (double firstNumber, double secondNumber)
+{
+    const double epsilon = 1e-9;
+
+    const double difference = firstNumber - secondNumber;
+
+    if (difference > epsilon)
+        return  1;
+    if (difference < -epsilon)
+        return -1;
+    
+    return 0;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+double dmin (double firstNumber, double secondNumber)
+{
+    if (differenceSign(firstNumber, secondNumber) >= 0)
+        return firstNumber;
+
+    return secondNumber;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+const char *simpleCommandLineParser (const int argc, const char *argv[])
+{
+    CHECKERROR(argv != NULL &&
+               "Argv pointer can't be NULL.",
+               NULL);
+
+    CHECKERROR(argc != 1 &&
+               "You haven't entered file name."
+               "Example: ./e++ aboba.awp", 
+               NULL);
+
+    CHECKWARNING(argc == 2 &&
+                 "You have entered more than two arguments."
+                 "Only second argument is taken as the filename.");
+
+    return argv[1];
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
